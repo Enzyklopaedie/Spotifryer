7af9887e537bcf04a80c172602db5e1371a17df3
Your Name <you@example.com>
2025-07-26 20:43:00 +0200

    Added the Spotify-like GUI

--- app/src/main/java/com/lateinlabs/spotifryer/MainActivity.kt
+++ app/src/main/java/com/lateinlabs/spotifryer/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.lateinlabs.spotifryer
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // This is the only line you really need in onCreate for now.
+        // It connects your code to your XML layout file.
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
